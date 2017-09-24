26f5f876e9fc554ce2875b7c3161396a81f1c2d7
zasprose <zijunzhang2015@gmail.com>
2017-09-24 14:12:47 -0700

    update mainactivity

--- app/src/main/java/com/mercari/mercaritest/MainActivity.java
+++ app/src/main/java/com/mercari/mercaritest/MainActivity.java
@@ -2,12 +2,52 @@ package com.mercari.mercaritest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AbsListView;
+import android.widget.GridView;
 
-public class MainActivity extends AppCompatActivity {
+import com.mercari.mercaritest.data.model.GridAdapter;
+import com.mercari.mercaritest.data.model.HomeResponse;
+
+public class MainActivity extends AppCompatActivity implements AbsListView.OnScrollListener{
+
+    private GridView grid;
+    private int THRESHOLD = 3;
+    private android.os.Handler mHandler = new android.os.Handler();
+    private GridAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // load json file
+        HomeResponse hr = new HomeResponse("all.json", this.getBaseContext());
+
+        // load gridView
+        grid = (GridView)findViewById(R.id.container);
+        mAdapter = new GridAdapter(this.getBaseContext(), hr.rp.data);
+
+        // set adapter
+        grid.setAdapter(mAdapter);
+        grid.setOnScrollListener(this);
+
     }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        mHandler.postDelayed(addItemRunn, 1000);
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        mHandler.postDelayed(addItemRunn, 1000);
+    }
+
+    // thread for dynamtic load adapter
+    private Runnable addItemRunn = new Runnable() {
+        @Override
+        public void run() {
+            mAdapter.addItems(5);
+        }
+    };
 }
--- app/src/main/java/com/mercari/mercaritest/data/model/GridAdapter.java
+++ app/src/main/java/com/mercari/mercaritest/data/model/GridAdapter.java
@@ -1,12 +1,29 @@
 package com.mercari.mercaritest.data.model;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.mercari.mercaritest.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static com.mercari.mercaritest.R.layout.grid_cell;
 
 /**
  * Created by zasprose on 9/24/17.
@@ -58,6 +75,91 @@ public class GridAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        // load new view
+        if(convertView==null || position<min || position>max || (up==true && position==min+1)) {
+            // scroll up and load
+            if(position<min || (up==true && position==min+1)) {
+                up = true;
+                down = false;
+                min--;
+                max--;
+            }
+            // scroll down and load
+            if(position>max) {
+                up = false;
+                down = true;
+                min++;
+                max++;
+            }
+            // load cell
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View cellView = new View(context);
+            cellView = inflater.inflate(grid_cell, null);
+
+            // set value
+            TextView priceTxt = (TextView) cellView.findViewById(R.id.itemPrice);
+            TextView nameTxt = (TextView) cellView.findViewById(R.id.itemName);
+            ImageView photoImg = (ImageView) cellView.findViewById(R.id.itemPhoto);
+
+            // price
+            priceTxt.setText("$"+Long.toString(data.get(position).price));
+
+            // name
+            nameTxt.setText(data.get(position).name);
+
+            // photo
+            String url = data.get(position).photo;
+            Bitmap bp1 = loadImgFromUrlFunc(url.substring(0,4)+"s"+url.substring(4), data.get(position).id);
+            Drawable dw1 = new BitmapDrawable(context.getResources(), Bitmap.createScaledBitmap(bp1, width/3-9, width/3-9, false));
+            photoImg.getLayoutParams().width = width/3-18;
+            photoImg.getLayoutParams().height = width/3-18;
+            photoImg.setBackground(dw1);
+
+            // sold out
+            if(data.get(position).status.equals("sold_out"))
+                photoImg.setImageResource(R.mipmap.sold);
+
+            return cellView;
+        } else {
+            // use exist view
+            return convertView;
+        }
+    }
+
+    // load bitmap from photo url
+    private Bitmap loadImgFromUrlFunc(String url, String name) {
+        Bitmap res = null;
+
+        try {
+            res = new loadImgFromUrl().execute(url).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return res;
+    }
+
+    // asynctask for loading photo from server
+    private static class loadImgFromUrl extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            //invalid input
+            if(params==null || params.length!=1)return null;
+            InputStream is = null;
+            Bitmap res = null;
+
+            try {
+                is = new URL(params[0]).openStream();
+                res = BitmapFactory.decodeStream(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return res;
+        }
+
     }
 }
