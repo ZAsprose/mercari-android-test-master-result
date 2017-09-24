a32b8c633b1c73032ae67283f9dd32f1113fcc5e
zasprose <zijunzhang2015@gmail.com>
2017-09-24 13:47:41 -0700

    GridAdapter

--- app/src/main/java/com/mercari/mercaritest/data/model/GridAdapter.java
+++ app/src/main/java/com/mercari/mercaritest/data/model/GridAdapter.java
@@ -1,17 +1,49 @@
 package com.mercari.mercaritest.data.model;
 
+import android.content.Context;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.List;
 
 /**
  * Created by zasprose on 9/24/17.
  */
 
 public class GridAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Item> data;
+    // width of screen
+    private int width;
+    // for dynamtic loading adapter
+    private int min, max, mCount;
+    private boolean up = false;
+    private boolean down = false;
+
+    public GridAdapter(Context context, List data) {
+        this.context = context;
+        this.data = data;
+        // calculate screen width
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        this.width = metrics.widthPixels;
+        // init variable
+        this.min = 0;
+        this.max = 3*(metrics.heightPixels/(width/3))-1;
+        this.mCount = max-min+1;
+    }
+
+    // dynamtic add loading item
+    public void addItems(int count) {
+        mCount += count;
+        notifyDataSetChanged();
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return mCount;
     }
 
     @Override
